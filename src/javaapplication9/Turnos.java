9e9d6037f1a0c3ad706bfdedca2394c327c24f0e
sena <sena@sena-MS-7817>
2017-05-03 11:16:50 -0500

    v.3

--- src/javaapplication9/Turnos.java
+++ src/javaapplication9/Turnos.java
@@ -10,5 +10,41 @@ package javaapplication9;
  * @author sena
  */
 public class Turnos {
+    private String idTurno;
+    private String opcionTurno;
+    private String numero;
+       
     
+    public void setIdTurno(String idTurnoP)
+    {
+    this.idTurno=idTurnoP;
+     }
+    public String getIdTurno ()
+    {
+        return this.idTurno;
+    }
+    
+    
+     public void setOpcionTurno(String opcionTurnoP)
+    {
+    this.opcionTurno=opcionTurnoP;
+     }
+    public String getOpcionTurno ()
+    {
+        return this.opcionTurno;
+    }
+    
+    
+     public void setNumero(String numeroP)
+    {
+    this.numero=numeroP;
+     }
+    public String getNumero ()
+    {
+        return this.numero;
+    }
+    
+    public void brindarAtencion(){
+        
+    }
 }
